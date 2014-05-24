c447ada44dbdd112aeb1396e8b80858665d4dcf6
Renan <renanigt@gmail.com>
2014-05-24 00:11:37 -0300

    Reestruturação do modelo.

---
+++ src/main/java/br/com/controledebeneficios/model/Beneficiadora.java
@@ -0,0 +1,24 @@
+package br.com.controledebeneficios.model;
+
+public class Beneficiadora {
+
+	private String nome;
+	private String tipoBeneficio;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipoBeneficio() {
+		return tipoBeneficio;
+	}
+
+	public void setTipoBeneficio(String tipoBeneficio) {
+		this.tipoBeneficio = tipoBeneficio;
+	}
+	
+}
--- src/main/java/br/com/controledebeneficios/model/Beneficio.java
+++ src/main/java/br/com/controledebeneficios/model/Beneficio.java
@@ -2,21 +2,21 @@ package br.com.controledebeneficios.model;
 
 public class Beneficio {
 
-	private String descricao;
+	private Beneficiadora beneficiadora;
 	private Integer pontos;
 	
-	public String getDescricao() {
-		return descricao;
+	public Beneficiadora getBeneficiadora() {
+		return beneficiadora;
 	}
-	
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+
+	public void setBeneficiadora(Beneficiadora beneficiadora) {
+		this.beneficiadora = beneficiadora;
 	}
-	
+
 	public Integer getPontos() {
 		return pontos;
 	}
-	
+
 	public void setPontos(Integer pontos) {
 		this.pontos = pontos;
 	}

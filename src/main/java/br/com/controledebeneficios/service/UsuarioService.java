fcdcbcaea07cec07973c845535bce82d9754f467
Renan <renanigt@gmail.com>
2014-07-06 02:16:49 -0300

    Adding DI by constructor.

--- src/main/java/br/com/controledebeneficios/service/UsuarioService.java
+++ src/main/java/br/com/controledebeneficios/service/UsuarioService.java
@@ -1,19 +1,15 @@
 package br.com.controledebeneficios.service;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
-
-import br.com.controledebeneficios.model.Usuario;
 
 public class UsuarioService {
 
 	private EntityManager manager;
 	
+	@Inject
 	public UsuarioService(EntityManager manager) {
 		this.manager = manager;
-	}
-	
-	public void salvar(Usuario usuario) {
-		manager.persist(usuario);
 	}
 
 }

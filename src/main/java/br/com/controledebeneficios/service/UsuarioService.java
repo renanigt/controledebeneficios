39551f73335608cbafb81c1af4856de0b8321b61
Renan <renanigt@gmail.com>
2014-07-07 23:37:29 -0300

    Adding initial integration tests withou DBunit.

--- src/main/java/br/com/controledebeneficios/service/UsuarioService.java
+++ src/main/java/br/com/controledebeneficios/service/UsuarioService.java
@@ -1,7 +1,12 @@
 package br.com.controledebeneficios.service;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.controledebeneficios.model.Usuario;
 
 public class UsuarioService {
 
@@ -12,4 +17,16 @@ public class UsuarioService {
 		this.manager = manager;
 	}
 
+	public void salvar(Usuario usuario) {
+		manager.persist(usuario);
+	}
+	
+	public List<Usuario> lista() {
+		String hql = "from Usuario order by login";
+		
+		Query query = manager.createQuery(hql);
+
+		return query.getResultList();
+	}
+	
 }
--- src/test/integration/br/com/controledebeneficios/service/UsuarioServiceTest.java
+++ src/test/integration/br/com/controledebeneficios/service/UsuarioServiceTest.java
@@ -2,28 +2,96 @@ package br.com.controledebeneficios.service;
 
 import java.io.File;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import br.com.caelum.vraptor.jpa.EntityManagerFactoryCreator;
 import br.com.controledebeneficios.model.Usuario;
 
 @RunWith(Arquillian.class)
 public class UsuarioServiceTest {
 
+	private EntityManager manager;
+	private EntityManagerFactory factory;
+	private EntityTransaction transaction;
+	
+	private UsuarioService service;
+	
 	@Deployment
 	public static JavaArchive createDeployment() {
 		return ShrinkWrap.create(JavaArchive.class)
-				.addClass(Usuario.class)
-				.addClass(UsuarioService.class)
-				.addClass(EntityManagerFactoryCreator.class)
 				.addAsResource(new File("src/test/resources/META-INF/persistence-test.xml"),
-						"persistence.xml")
-				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+						"META-INF/persistence.xml");
+	}
+	
+	@Before
+	public void setUp() {
+		configure();
+		deleteData();
+		startTransaction();
+		
+		service = new UsuarioService(manager);
+	}
+
+	@Test
+	public void deveriaSalvarUsuario() {
+		Usuario usuario = createUsuario();
+		service.salvar(usuario);
+		
+		commitTransaction();
+		
+		Assert.assertEquals(1, service.lista().size());
+	}
+	
+	private Usuario createUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setLogin("renanigt");
+		usuario.setSenha("teste123");
+		
+		return usuario;
+	}
+	
+	@After
+	public void tearDown() {
+		deleteData();
+		closeFactoryAndManager();
+	}
+	
+	private void configure() {
+		factory = Persistence.createEntityManagerFactory("default-test");
+		manager = factory.createEntityManager();
+		transaction = manager.getTransaction();
+	}
+	
+	private void startTransaction() {
+		transaction.begin();
+	}
+
+	private void deleteData() {
+		transaction.begin();
+		manager.createQuery("delete from Usuario").executeUpdate();
+		transaction.commit();
+	}
+
+	private void closeFactoryAndManager() {
+		manager.close();
+		factory.close();
+	}
+	
+	private void commitTransaction() {
+		transaction.commit();
 	}
 	
 }

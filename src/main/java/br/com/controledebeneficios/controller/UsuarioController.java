201d40d2fe68d6df7560a40bdcec1735dddc7b6e
Renan <renanigt@gmail.com>
2014-07-15 00:01:07 -0300

    Adding UsuarioController and its tests.

--- src/main/java/br/com/controledebeneficios/controller/UsuarioController.java
+++ src/main/java/br/com/controledebeneficios/controller/UsuarioController.java
@@ -1,20 +1,30 @@
 package br.com.controledebeneficios.controller;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 import javax.inject.Inject;
 
+import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.SimpleMessage;
+import br.com.caelum.vraptor.validator.Validator;
+import br.com.controledebeneficios.model.Usuario;
 import br.com.controledebeneficios.service.UsuarioService;
 
+@Controller
 public class UsuarioController {
 
 	private Result result;
+	private Validator validator;
 	
 	private UsuarioService service;
 	
 	@Inject
-	public UsuarioController(Result result, UsuarioService service) {
+	public UsuarioController(Result result, Validator validator, UsuarioService service) {
 		this.result = result;
+		this.validator = validator;
 		this.service = service;
 	}
 	
@@ -27,6 +37,23 @@ public class UsuarioController {
 	@Get("/usuario")
 	public void index() {
 		result.include("usuarios", service.lista());
+	}
+
+	@Get("/usuario/novo")
+	public void novo() {
+		
+	}
+	
+	@Post("/usuario/novo/salvar")
+	public void adiciona(Usuario usuario) {
+		validator.addIf(isNullOrEmpty(usuario.getLogin()), new SimpleMessage("Login", "O Login não pode ser vazio."));
+		validator.addIf(isNullOrEmpty(usuario.getSenha()), new SimpleMessage("Senha", "A Senha não pode ser vazia."));
+
+		validator.onErrorRedirectTo(this).novo();
+		
+		service.salvar(usuario);
+		
+		result.include("sucesso", "Usuário adicionado com sucesso.");
 	}
 	
 }
--- src/test/unit/br/com/controledebeneficios/controller/UsuarioControllerTest.java
+++ src/test/unit/br/com/controledebeneficios/controller/UsuarioControllerTest.java
@@ -1,17 +1,23 @@
 package br.com.controledebeneficios.controller;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import br.com.caelum.vraptor.util.test.MockResult;
+import br.com.caelum.vraptor.util.test.MockValidator;
+import br.com.caelum.vraptor.validator.ValidationException;
 import br.com.controledebeneficios.builder.UsuarioBuilder;
 import br.com.controledebeneficios.model.Usuario;
 import br.com.controledebeneficios.service.UsuarioService;
@@ -19,6 +25,7 @@ import br.com.controledebeneficios.service.UsuarioService;
 public class UsuarioControllerTest {
 
 	private MockResult result;
+	private MockValidator validator;
 	@Mock
 	private UsuarioService service;
 	
@@ -31,21 +38,41 @@ public class UsuarioControllerTest {
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
 		result = new MockResult();
+		validator = new MockValidator();
 
-		controller = new UsuarioController(result, service);
+		controller = new UsuarioController(result, validator, service);
 		
 		usuarioRenan = new UsuarioBuilder().comId(1).comLogin("renanigt").comSenha("teste123").build();
 		usuarioIago= new UsuarioBuilder().comId(2).comLogin("iagota").comSenha("321teste").build();
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Test
 	public void deveriaAbrirInicialComTodosUsuarios() {
-		Mockito.when(service.lista()).thenReturn(Arrays.asList(usuarioRenan, usuarioIago));
+		when(service.lista()).thenReturn(Arrays.asList(usuarioRenan, usuarioIago));
 		
 		controller.index();
 		
 		assertTrue("Deve conter lista de usuários", result.included().containsKey("usuarios"));
-		assertEquals(Arrays.asList(usuarioRenan, usuarioIago), result.included("usuarios"));
+		Assert.assertThat((List<Usuario>) result.included("usuarios"), containsInAnyOrder(usuarioRenan, usuarioIago));
+	}
+
+	@Test
+	public void deveriaAdicionarUsuario() {
+		Usuario usuario = new UsuarioBuilder().comLogin("fulano").comSenha("fulano123").build();
+		
+		controller.adiciona(usuario);
+		
+		verify(service).salvar(usuario);
+		
+		Assert.assertThat(result.included("sucesso").toString(), is("Usuário adicionado com sucesso."));
+	}
+	
+	@Test(expected = ValidationException.class)
+	public void naoDeveriaSalvarUsuarioSemDados() {
+		Usuario usuario = new UsuarioBuilder().comLogin("").comSenha("").build();
+		
+		controller.adiciona(usuario);
 	}
 	
 }

38aa6a139984b6dfbab25c711e5b08aefcb6cc37
Renan <renanigt@gmail.com>
2014-07-27 22:51:42 -0300

    Adding method to add Beneficiadora.

--- src/main/java/br/com/controledebeneficios/controller/BeneficiadoraController.java
+++ src/main/java/br/com/controledebeneficios/controller/BeneficiadoraController.java
@@ -2,32 +2,52 @@ package br.com.controledebeneficios.controller;
 
 import javax.inject.Inject;
 
+import com.google.common.base.Strings;
+
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.SimpleMessage;
+import br.com.caelum.vraptor.validator.Validator;
+import br.com.controledebeneficios.model.Beneficiadora;
 import br.com.controledebeneficios.service.BeneficiadoraService;
 
 public class BeneficiadoraController {
 
 	private Result result;
+	private Validator validator;
 	
 	private BeneficiadoraService service;
 	
 	@Inject
-	public BeneficiadoraController(Result result, BeneficiadoraService service) {
+	public BeneficiadoraController(Result result, Validator validator, BeneficiadoraService service) {
 		this.result = result;
 		this.service = service;
+		this.validator = validator;
 	}
 	
 	/**
 	 * @deprecated CDI eyes only
 	 */
 	public BeneficiadoraController() {
-		this(null, null);
+		this(null, null, null);
 	}
 	
 	@Get("/beneficiadora")
 	public void index() {
 		result.include("beneficiadoras", service.lista());
+	}
+
+	@Post("/beneficiadora/novo/salvar")
+	public void adiciona(Beneficiadora beneficiadora) {
+		validator.addIf(Strings.isNullOrEmpty(beneficiadora.getNome()), new SimpleMessage("Nome", "Nome não pode ser vazio."));
+		validator.addIf(beneficiadora.getTipoBeneficio() == null, new SimpleMessage("Tipo de Beneficio", "Tipo de Beneficio não pode ser vazio."));
+		
+		validator.onErrorForwardTo(this).index();
+		
+		service.salva(beneficiadora);
+		
+		result.include("sucesso", "Beneficiadora adicionada com sucesso.");
 	}
 
 }
--- src/test/unit/br/com/controledebeneficios/controller/BeneficiadoraControllerTest.java
+++ src/test/unit/br/com/controledebeneficios/controller/BeneficiadoraControllerTest.java
@@ -1,6 +1,7 @@
 package br.com.controledebeneficios.controller;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
@@ -11,9 +12,12 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import br.com.caelum.vraptor.util.test.MockResult;
+import br.com.caelum.vraptor.util.test.MockValidator;
+import br.com.caelum.vraptor.validator.ValidationException;
 import br.com.controledebeneficios.builder.BeneficiadoraBuilder;
 import br.com.controledebeneficios.model.Beneficiadora;
 import br.com.controledebeneficios.model.TipoBeneficio;
@@ -22,6 +26,7 @@ import br.com.controledebeneficios.service.BeneficiadoraService;
 public class BeneficiadoraControllerTest {
 
 	private MockResult result;
+	private MockValidator validator;
 	@Mock
 	private BeneficiadoraService service;
 	
@@ -34,7 +39,8 @@ public class BeneficiadoraControllerTest {
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
 		result = new MockResult();
-		controller = new BeneficiadoraController(result, service); 
+		validator = new MockValidator();
+		controller = new BeneficiadoraController(result, validator, service); 
 		
 		aleloRefeicao = new BeneficiadoraBuilder().comNome("Alelo Refeição").comTipoBeneficio(TipoBeneficio.VALE_REFEICAO).build();
 		unimed = new BeneficiadoraBuilder().comNome("Unimed").comTipoBeneficio(TipoBeneficio.PLANO_SAUDE).build();
@@ -49,6 +55,25 @@ public class BeneficiadoraControllerTest {
 		
 		assertTrue("Deve conter lista de beneficiadoras", result.included().containsKey("beneficiadoras"));
 		assertThat((List<Beneficiadora>) result.included("beneficiadoras"), containsInAnyOrder(unimed, aleloRefeicao));
+	}
+	
+	@Test
+	public void deveriaAdicionarUmaBeneficiadora() {
+		Beneficiadora beneficiadora = new BeneficiadoraBuilder().comNome("Uniodonto").
+				comTipoBeneficio(TipoBeneficio.PLANO_ODONTOLOGICO).build();
+		
+		controller.adiciona(beneficiadora);
+		
+		Mockito.verify(service).salva(beneficiadora);
+		
+		assertThat(result.included("sucesso").toString(), is("Beneficiadora adicionada com sucesso."));
+	}
+	
+	@Test(expected = ValidationException.class)
+	public void naoDeveriaSalvarBeneficiadoraSemDados() {
+		Beneficiadora beneficiadora = new BeneficiadoraBuilder().comNome("").comTipoBeneficio(null).build();
+		
+		controller.adiciona(beneficiadora);
 	}
 	
 }
